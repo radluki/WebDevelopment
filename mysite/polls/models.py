aba71f255edcdf8e5234770cfdd30e2aab5a933c
radluki <radluki@gmail.com>
2020-03-12 21:08:06 +0100

    views with params, admin for polls

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -6,6 +6,9 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return f"<{self.question_text}>"
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
