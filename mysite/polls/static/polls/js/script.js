ecb3c1ea78961ca2bc6e05d6208c973364f22cea
radluki <radluki@gmail.com>
2020-03-14 13:04:54 +0100

    Chess board created by java script

--- mysite/polls/static/polls/js/script.js
+++ mysite/polls/static/polls/js/script.js
@@ -1,5 +1,19 @@
-function printHelloWorld() {
-    console.log("Hello World");
-}
 
-printHelloWorld();
+function createChessBoard() {
+    var board = document.createElement("div")
+    for(let r=0; r<8; r++){
+        var row = document.createElement("div")
+        row.style.width = "400px"
+        for(let c=0; c<8; c++){
+            var field = document.createElement("div")
+            field.style.background = (c + r)%2==0 ? "black" : "white"
+            field.style.height = "50px"
+            field.style.width = "50px"
+            field.style.float = "left"
+            field.onclick = function() {console.log(c,r)}
+            row.appendChild(field)
+        }
+        board.appendChild(row)
+    }
+    document.body.appendChild(board)
+}
