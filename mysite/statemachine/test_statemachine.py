680ea9e4294b2cb1f920249b22cc99d5266bc6b1
radluki <radluki@gmail.com>
2020-03-16 21:38:38 +0100

    Test refactor 2

--- mysite/statemachine/test_statemachine.py
+++ mysite/statemachine/test_statemachine.py
@@ -7,17 +7,35 @@ def check_event(machine, event, target_state):
     assert machine.state.__class__ == target_state
 
 
-def test_state_machine():
-    initial_state = StateTwo()
-    sm = StateMachine(initial_state)
+@pytest.fixture
+def machine_one():
+    return StateMachine(State())
 
-    event = 10
-    check_event(sm, event, State)
+
+@pytest.fixture
+def machine_two():
+    return StateMachine(StateTwo())
+
+
+def test_state_one_string(machine_one):
     event = "string"
-    check_event(sm, event, State)
-    event = 11
-    check_event(sm, event, StateTwo)
-    check_event(sm, event, State)
+    check_event(machine_one, event, State)
+    assert machine_one.state._priv.string == event
+
+
+def test_state_one_number(machine_one):
+    event = 10
+    check_event(machine_one, event, StateTwo)
+    assert not machine_one.state._priv.__dict__
+
+
+def test_state_one_dummy_class(machine_one):
     event = DummyClass()
-    check_event(sm, event, State)
-    assert sm.state._priv.dummy.__class__ == DummyClass
+    check_event(machine_one, event, State)
+    assert machine_one.state._priv.dummy == event
+
+
+def test_state_two_number(machine_two):
+    event = 10
+    check_event(machine_two, event, State)
+    assert not machine_two.state._priv.__dict__
